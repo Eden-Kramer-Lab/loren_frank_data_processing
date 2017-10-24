7533fdcb2b0f3f73c155f7c10f9e3a36e955b2ae
Eric Denovellis <edeno@bu.edu>
2017-10-24 14:18:29 -0400

    Add files for pip/conda/continuous intergration

---
+++ setup.py
@@ -0,0 +1,19 @@
+#!/usr/bin/env python3
+
+from setuptools import setup, find_packages
+
+INSTALL_REQUIRES = ['numpy >= 1.11', 'pandas >= 0.18.0', 'scipy', 'xarray']
+TESTS_REQUIRE = ['pytest >= 2.7.1']
+
+setup(
+    name='loren_frank_data_processing',
+    version='0.1.0.dev0',
+    license='GPL-3.0',
+    description=('Import data from Loren Frank lab'),
+    author='Eric Denovellis',
+    author_email='edeno@bu.edu',
+    url='https://github.com/Eden-Kramer-Lab/loren_frank_data_processing',
+    packages=find_packages(),
+    install_requires=INSTALL_REQUIRES,
+    tests_require=TESTS_REQUIRE,
+)
