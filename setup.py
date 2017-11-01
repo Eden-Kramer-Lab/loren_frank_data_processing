f05645392db38b1826cfeb44389b92c26aa7f478
Eric Denovellis <edeno@bu.edu>
2017-11-01 11:03:40 -0400

    Add dask as a dependency

--- setup.py
+++ setup.py
@@ -2,8 +2,8 @@
 
 from setuptools import find_packages, setup
 
-INSTALL_REQUIRES = ['numpy >= 1.11',
-                    'pandas >= 0.18.0', 'scipy', 'xarray', 'netcdf4']
+INSTALL_REQUIRES = ['numpy >= 1.11', 'pandas >= 0.18.0', 'scipy', 'xarray',
+                    'netcdf4', 'dask']
 TESTS_REQUIRE = ['pytest >= 2.7.1']
 
 setup(
