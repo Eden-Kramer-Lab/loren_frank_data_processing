63e1d94d5782827cfd45b08a27a6308237a6ec8b
Eric Denovellis <edeno@bu.edu>
2017-12-06 16:34:46 -0500

    Add track segement classifier and linearized position

---
+++ loren_frank_data_processing/position/position.py
@@ -0,0 +1,167 @@
+import numpy as np
+import pandas as pd
+
+from ..core import get_data_structure
+from ..tetrodes import get_trial_time
+from .track_segment_classification import (calculate_linear_distance,
+                                           classify_track_segments,
+                                           make_track_graph)
+from .well_traversal_classification import score_inbound_outbound, segment_path
+
+
+def get_position_dataframe(epoch_key, animals):
+    '''Returns a list of position dataframes with a length corresponding
+     to the number of epochs in the epoch key -- either a tuple or a
+    list of tuples with the format (animal, day, epoch_number)
+
+    Parameters
+    ----------
+    epoch_key : tuple
+        Unique key identifying a recording epoch. Elements are
+        (animal, day, epoch)
+    animals : dict of named-tuples
+        Dictionary containing information about the directory for each
+        animal. The key is the animal_short_name.
+
+    Returns
+    -------
+    position : pandas dataframe
+        Contains information about the animal's position, head direction,
+        and speed.
+
+    '''
+    animal, day, epoch = epoch_key
+    struct = get_data_structure(animals[animal], day, 'pos', 'pos')[epoch - 1]
+    position_data = struct['data'][0, 0]
+    field_names = struct['fields'][0, 0].item().split()
+    NEW_NAMES = {'x': 'x_position',
+                 'y': 'y_position',
+                 'dir': 'head_direction',
+                 'vel': 'speed'}
+    time = pd.TimedeltaIndex(
+        position_data[:, field_names.index('time')], unit='s', name='time')
+    return (pd.DataFrame(
+        position_data, columns=field_names, index=time)
+        .rename(columns=NEW_NAMES)
+        .drop([name for name in field_names
+               if name not in NEW_NAMES], axis=1))
+
+
+def get_linear_position_structure(epoch_key, animals):
+    '''The time series of linearized (1D) positions of the animal for a given
+    epoch.
+
+    Parameters
+    ----------
+    epoch_key : tuple
+        Unique key identifying a recording epoch. Elements are
+        (animal, day, epoch)
+    animals : dict of named-tuples
+        Dictionary containing information about the directory for each
+        animal. The key is the animal_short_name.
+
+    Returns
+    -------
+    linear_position : pandas.DataFrame
+
+    '''
+    animal, day, epoch = epoch_key
+    struct = get_data_structure(
+        animals[animal], day, 'linpos', 'linpos')[epoch - 1][0][0][
+            'statematrix']
+    INCLUDE_FIELDS = ['traj', 'lindist']
+    time = pd.TimedeltaIndex(struct['time'][0][0].flatten(), unit='s',
+                             name='time')
+    new_names = {'time': 'time', 'traj': 'trajectory_category_ind',
+                 'lindist': 'linear_distance'}
+    data = {new_names[name]: struct[name][0][0].flatten()
+            for name in struct.dtype.names
+            if name in INCLUDE_FIELDS}
+    return pd.DataFrame(data, index=time)
+
+
+def get_interpolated_position_dataframe(epoch_key, animals,
+                                        time_function=get_trial_time,
+                                        max_distance_from_well=15):
+    '''Gives the interpolated position of animal for a given epoch.
+
+    Defaults to interpolating the position to the LFP time. Can use the
+    `time_function` to specify different time to interpolate to.
+
+    Parameters
+    ----------
+    epoch_key : tuple
+    animals : dict of named-tuples
+        Dictionary containing information about the directory for each
+        animal. The key is the animal_short_name.
+    time_function : function, optional
+        Function that take an epoch key (animal_short_name, day, epoch) that
+        defines the time the multiunits are relative to. Defaults to using
+        the time the LFPs are sampled at.
+
+    Returns
+    -------
+    interpolated_position : pandas.DataFrame
+
+    '''
+    time = time_function(epoch_key, animals)
+    position_df = get_position_dataframe(epoch_key, animals)
+
+    track_graph, center_well_id = make_track_graph(epoch_key, animals)
+    position = position_df.loc[:, ['x_position', 'y_position']].values
+    track_segment_id = classify_track_segments(
+        track_graph, position,
+        route_euclidean_distance_scaling=1)
+    linear_distance = calculate_linear_distance(
+        track_graph, track_segment_id, center_well_id, position)
+    position_df['linear_distance'] = linear_distance
+
+    old_dt = (position_df.index[1] - position_df.index[0]).total_seconds()
+
+    well_locations = get_well_locations(epoch_key, animals)
+    segments_df, labeled_segments = segment_path(
+        position_df.index, position, well_locations,
+        max_distance_from_well=max_distance_from_well)
+    segments_df = score_inbound_outbound(segments_df).loc[
+        :, ['from_well', 'to_well', 'task', 'is_correct']]
+
+    segments_df = pd.merge(
+        labeled_segments, segments_df, right_index=True,
+        left_on='labeled_segments', how='outer')
+    position_df = pd.concat((position_df, segments_df), axis=1)
+
+    categorical_columns = ['labeled_segments', 'from_well', 'to_well', 'task',
+                           'is_correct']
+    continuous_columns = ['head_direction', 'speed', 'linear_distance',
+                          'x_position', 'y_position']
+    position_categorical = (position_df
+                            .drop(continuous_columns, axis=1)
+                            .reindex(index=time, method='pad'))
+    position_continuous = position_df.drop(categorical_columns, axis=1)
+    new_index = pd.Index(np.unique(np.concatenate(
+        (position_continuous.index, time))), name='time')
+    interpolated_position = (position_continuous
+                             .reindex(index=new_index)
+                             .interpolate(method='values')
+                             .reindex(index=time))
+    interpolated_position.loc[
+        interpolated_position.linear_distance < 0, 'linear_distance'] = 0
+    interpolated_position.loc[interpolated_position.speed < 0, 'speed'] = 0
+    limit = np.ceil(old_dt / (time[1] - time[0]).total_seconds()).astype(int)
+    return (pd.concat([position_categorical, interpolated_position], axis=1)
+            .fillna(method='backfill', limit=limit))
+
+
+def get_well_locations(epoch_key, animals):
+    '''Retrieves the 2D coordinates for each well.
+    '''
+    animal, day, epoch = epoch_key
+    task_file = get_data_structure(animals[animal], day, 'task', 'task')
+    linearcoord = task_file[epoch - 1]['linearcoord'][0, 0].squeeze()
+    well_locations = []
+    for arm in linearcoord:
+        well_locations.append(arm[0, :, 0])
+        well_locations.append(arm[-1, :, 0])
+    well_locations = np.stack(well_locations)
+    _, ind = np.unique(well_locations, axis=0, return_index=True)
+    return well_locations[np.sort(ind), :]
---
+++ loren_frank_data_processing/position/track_segment_classification.py
@@ -0,0 +1,433 @@
+from itertools import product
+
+import numpy as np
+
+import networkx as nx
+
+from ..core import get_data_structure
+
+np.warnings.filterwarnings('ignore')
+
+
+def get_track_segments(epoch_key, animals):
+    '''
+
+    Parameters
+    ----------
+    epoch_key : tuple
+    animals : dict of namedtuples
+
+    Returns
+    -------
+    track_segments : ndarray, shape (n_segments, n_nodes, n_space)
+    center_well_position : ndarray, shape (n_space,)
+
+    '''
+    animal, day, epoch = epoch_key
+    task_file = get_data_structure(animals[animal], day, 'task', 'task')
+    linearcoord = task_file[epoch - 1]['linearcoord'][0, 0].squeeze()
+    track_segments = [np.stack(((arm[:-1, :, 0], arm[1:, :, 0])), axis=1)
+                      for arm in linearcoord]
+    center_well_position = track_segments[0][0][0]
+    return (np.unique(np.concatenate(track_segments), axis=0),
+            center_well_position)
+
+
+def make_track_graph(epoch_key, animals):
+    '''
+
+    Parameters
+    ----------
+    epoch_key : tuple, (animal, day, epoch)
+    animals : dict of namedtuples
+
+    Returns
+    -------
+    track_graph : networkx Graph
+    center_well_id : int
+
+    '''
+    track_segments, center_well_position = get_track_segments(
+        epoch_key, animals)
+    nodes = np.unique(track_segments.reshape((-1, 2)), axis=0)
+
+    edges = np.zeros(track_segments.shape[:2], dtype=int)
+    for node_id, node in enumerate(nodes):
+        edge_ind = np.nonzero(np.isin(track_segments, node).sum(axis=2) > 1)
+        edges[edge_ind] = node_id
+
+    edge_distances = np.linalg.norm(
+        np.diff(track_segments, axis=-2).squeeze(), axis=1)
+
+    track_graph = nx.Graph()
+
+    for node_id, node_position in enumerate(nodes):
+        track_graph.add_node(node_id, pos=tuple(node_position))
+
+    for edge, distance in zip(edges, edge_distances):
+        track_graph.add_edge(edge[0], edge[1], distance=distance)
+
+    center_well_id = np.unique(
+        np.nonzero(np.isin(nodes, center_well_position).sum(axis=1) > 1)[0])[0]
+
+    return track_graph, center_well_id
+
+
+def get_track_segments_from_graph(track_graph):
+    '''
+
+    Parameters
+    ----------
+    track_graph : networkx Graph
+
+    Returns
+    -------
+    track_segments : ndarray, shape (n_segments, n_nodes, n_space)
+
+    '''
+    node_positions = nx.get_node_attributes(track_graph, 'pos')
+    return np.asarray([(node_positions[node1], node_positions[node2])
+                       for node1, node2 in track_graph.edges()])
+
+
+def plot_track(track_graph, ax=None):
+    '''
+
+    Parameters
+    ----------
+    track_graph : networkx Graph
+
+    '''
+    node_position = nx.get_node_attributes(track_graph, 'pos')
+    nx.draw_networkx(track_graph, node_position, ax)
+
+
+def project_points_to_segment(track_segments, position):
+    '''Finds the closet point on a track segment in terms of Euclidean distance
+
+    Parameters
+    ----------
+    track_segments : ndarray, shape (n_segments, n_nodes, n_space)
+    position : ndarray, shape (n_time, n_space)
+
+    Returns
+    -------
+    projected_positions : ndarray, shape (n_time, n_segments, n_space)
+
+    '''
+    segment_diff = np.diff(track_segments, axis=1).squeeze()
+    sum_squares = np.sum(segment_diff ** 2, axis=1)
+    node1 = track_segments[:, 0, :]
+    nx = (np.sum(segment_diff *
+                 (position[:, np.newaxis, :] - node1), axis=2) /
+          sum_squares)
+    nx[np.where(nx < 0)] = 0.0
+    nx[np.where(nx > 1)] = 1.0
+    return node1[np.newaxis, ...] + (
+        nx[:, :, np.newaxis] * segment_diff[np.newaxis, ...])
+
+
+def find_projected_point_distance(track_segments, position):
+    '''
+    '''
+    return np.linalg.norm(
+        position[:, np.newaxis, :] -
+        project_points_to_segment(track_segments, position), axis=2)
+
+
+def find_nearest_segment(track_segments, position):
+    '''Returns the track segment that is closest to the position
+    at each time point.
+
+    Parameters
+    ----------
+    track_segments : ndarray, shape (n_segments, n_nodes, n_space)
+    position : ndarray, shape (n_time, n_space)
+
+    Returns
+    -------
+    segment_id : ndarray, shape (n_time,)
+
+    '''
+    distance = find_projected_point_distance(track_segments, position)
+    return np.argmin(distance, axis=1)
+
+
+def euclidean_distance(position):
+    '''Distance between position at successive time points
+
+    Parameters
+    ----------
+    position : ndarray, shape (n_time, n_space)
+
+    Returns
+    -------
+    distance : ndarray, shape (n_time,)
+
+    '''
+    distance = np.linalg.norm(position[1:] - position[:-1], axis=1)
+    return np.concatenate(([np.nan], distance))
+
+
+def route_distance(candidates_t_1, candidates_t, track_graph):
+    '''
+
+    Parameters
+    ----------
+    candidates_t_1 : ndarray, shape (n_segments, n_space)
+    candidates_t : ndarray, shape (n_segments, n_space)
+    track_graph : networkx Graph
+
+    Returns
+    -------
+    route_distance : ndarray, shape (n_segments, n_segments)
+
+    '''
+    n_segments = len(track_graph.edges)
+    if np.any(np.isnan(candidates_t) | np.isnan(candidates_t)):
+        return np.full((n_segments, n_segments), np.nan)
+    track_graph1 = track_graph.copy()
+
+    # insert virtual node
+    for candidate_id, (position_t, position_t_1, edge_id) in enumerate(
+            zip(candidates_t, candidates_t_1, track_graph.edges)):
+        node_name_t = 't_{0}'.format(candidate_id)
+        node_name_t_1 = 't_1_{0}'.format(candidate_id)
+        node1, node2 = edge_id
+        outside_nodes = np.array([node1, node2], dtype=object)
+        inside_nodes = np.array([node_name_t, node_name_t_1], dtype=object)
+        outside_pos = np.array(
+            [track_graph1.nodes[node1]['pos'],
+             track_graph1.nodes[node2]['pos']], dtype=object)
+        inside_pos = np.array([position_t, position_t_1])
+        sorted_outside = np.argsort(outside_pos, axis=0)[:, 0]
+        sorted_inside = np.argsort(inside_pos, axis=0)[:, 0]
+        nodes = np.empty((4,), dtype=object)
+        nodes[[1, 2]] = inside_nodes[sorted_inside]
+        nodes[[0, 3]] = outside_nodes[sorted_outside]
+        track_graph1.add_path(nodes)
+        track_graph1.remove_edge(node1, node2)
+        track_graph1.nodes[node_name_t]['pos'] = tuple(position_t)
+        track_graph1.nodes[node_name_t_1]['pos'] = tuple(position_t_1)
+
+    # calculate distance
+    for e in track_graph1.edges(data=True):
+        track_graph1.edges[e[:2]]['distance'] = np.linalg.norm(
+            track_graph1.node[e[0]]['pos'] -
+            np.array(track_graph1.node[e[1]]['pos']))
+
+    # calculate path distance
+    node_names_t = ['t_{0}'.format(i) for i in range(n_segments)]
+    node_names_t_1 = ['t_1_{0}'.format(i) for i in range(n_segments)]
+    path_distance = [
+        nx.shortest_path_length(track_graph1, source=node_t, target=node_t_1,
+                                weight='distance')
+        for node_t, node_t_1 in product(node_names_t, node_names_t_1)]
+    return np.array(path_distance).reshape((n_segments, n_segments))
+
+
+def route_distances(position, track_graph):
+    track_segments = get_track_segments_from_graph(track_graph)
+    projected_track_position = project_points_to_segment(
+        track_segments, position)
+    distances = np.stack(
+        [route_distance(p_t, p_t_1, track_graph)
+         for p_t, p_t_1 in zip(projected_track_position[1:],
+                               projected_track_position[:-1])])
+    return np.concatenate(
+        (np.full((1, *distances.shape[1:]), np.nan), distances))
+
+
+def calculate_position_likelihood(position, track_graph, sigma=10):
+    track_segments = get_track_segments_from_graph(track_graph)
+    projected_position_distance = find_projected_point_distance(
+        track_segments, position)
+    return (np.exp(-0.5 * (projected_position_distance / sigma) ** 2) /
+            (np.sqrt(2 * np.pi) * sigma))
+
+
+def normalize_to_probability(x, axis=-1):
+    '''Ensure the array axis sum to 1
+
+    Parameters
+    ----------
+    x : ndarray
+
+    Returns
+    -------
+    normalized_x : ndarray
+
+    '''
+    return x / x.sum(axis=axis, keepdims=True)
+
+
+def calculate_empirical_state_transition(position, track_graph,
+                                         scaling=1E-1):
+    '''Calculates the state transition probabilty between track segments by
+    favoring route distances that are similar to euclidean distances between
+    successive time points.
+
+    Parameters
+    ----------
+    position : ndarray, shape (n_time, n_space)
+    track_graph : networkx Graph
+    scaling : float, optional
+
+    Returns
+    -------
+    state_transition_matrix : shape (n_time, n_segments, n_segments)
+
+    References
+    ----------
+    .. [1] Newson, P., and Krumm, J. (2009). Hidden Markov map matching through
+    noise and sparseness. In Proceedings of the 17th ACM SIGSPATIAL
+    International Conference on Advances in Geographic Information Systems,
+    (ACM), pp. 336-343.
+
+    '''
+    route_and_euclidean_distance_similarity = np.abs(
+        route_distances(position, track_graph) -
+        euclidean_distance(position)[:, np.newaxis, np.newaxis])
+    exponential_pdf = (
+        np.exp(-route_and_euclidean_distance_similarity / scaling) / scaling)
+    return normalize_to_probability(exponential_pdf, axis=2)
+
+
+def viterbi(initial_conditions, state_transition, likelihood):
+    '''Find the most likely sequence of paths using the Viterbi algorithm.
+
+    Note that the state_transition matrix is time-dependent. NaNs are removed
+    and placed back in at the end.
+
+    Parameters
+    ----------
+    initial_conditions : ndarray, shape (n_states,)
+    state_transition : ndarray, shape (n_time, n_states, n_states)
+    likelihood : ndarray, shape (n_time, n_states)
+
+    Returns
+    -------
+    state_id : ndarray, shape (n_time,)
+
+    '''
+    is_nan = np.any(np.isnan(likelihood), axis=1)
+    likelihood = likelihood.copy()[~is_nan]
+    state_transition = state_transition.copy()[~is_nan]
+
+    n_time, n_states = likelihood.shape
+    posterior = np.zeros((n_time, n_states))
+    max_state_ind = np.zeros((n_time, n_states), dtype=np.int)
+
+    # initialization
+    posterior[0] = np.log(initial_conditions) + np.log(likelihood[0])
+
+    # recursion
+    for time_ind in range(1, n_time):
+        prior = posterior[time_ind - 1] + np.log(state_transition[time_ind])
+        max_state_ind[time_ind] = prior.argmax(axis=1)
+        posterior[time_ind] = prior[np.arange(
+            n_states), max_state_ind[time_ind]] + np.log(likelihood[time_ind])
+
+    # termination
+    most_probable_state_ind = np.zeros((n_time,), dtype=np.int)
+    most_probable_state_ind[n_time - 1] = np.argmax(posterior[n_time - 1])
+
+    # path back-tracking
+    for time_ind in reversed(range(n_time - 1)):
+        most_probable_state_ind[time_ind] = max_state_ind[
+            time_ind + 1, most_probable_state_ind[time_ind + 1]]
+
+    most_probable_state_ind_with_nan = np.full((is_nan.size,), np.nan)
+    most_probable_state_ind_with_nan[~is_nan] = most_probable_state_ind
+    return most_probable_state_ind_with_nan
+
+
+def classify_track_segments(track_graph, position, sensor_std_dev=10,
+                            route_euclidean_distance_scaling=1E-1):
+    '''Find the most likely track segment for a given position.
+
+    Tries to make sure the euclidean distance between successive time points
+    is similar to the route distance along the graph.
+
+    Parameters
+    ----------
+    track_graph : networkx Graph
+    position : ndarray, shape (n_time, n_space)
+    sensor_std_dev : float, optional
+        Uncertainty of position sensor.
+    route_euclidean_distance_scaling : float, optional
+        How much to prefer route distances between successive time points
+        that are closer to the euclidean distance. Smaller numbers mean the
+        route distance is more likely to be close to the euclidean distance.
+
+    Returns
+    -------
+    segment_id : ndarray, shape (n_time,)
+
+    References
+    ----------
+    .. [1] Newson, P., and Krumm, J. (2009). Hidden Markov map matching through
+    noise and sparseness. In Proceedings of the 17th ACM SIGSPATIAL
+    International Conference on Advances in Geographic Information Systems,
+    (ACM), pp. 336-343.
+
+    '''
+    n_segments = len(track_graph.edges)
+    initial_conditions = np.ones((n_segments,))
+    state_transition = calculate_empirical_state_transition(
+        position, track_graph, scaling=route_euclidean_distance_scaling)
+    likelihood = calculate_position_likelihood(
+        position, track_graph, sigma=sensor_std_dev)
+
+    return viterbi(initial_conditions, state_transition, likelihood)
+
+
+def calculate_linear_distance(track_graph, track_segment_id, well_id,
+                              position):
+    '''Finds the path distance along a graph relative to a node.
+
+    Parameters
+    ----------
+    track_graph : networkx Graph
+    track_segment_id : ndarray, shape (n_time,)
+    well_id : hashable object
+    position : ndarray, shape (n_time, n_spaces)
+
+    Returns
+    -------
+    linear_distance : ndarray, shape (n_time,)
+        Linear distance from well specified by `well_id`
+
+    '''
+    track_segment_id[np.isnan(track_segment_id)] = 0
+    track_segment_id = track_segment_id.astype(int)
+
+    track_segments = get_track_segments_from_graph(track_graph)
+    projected_track_positions = project_points_to_segment(
+        track_segments, position)
+    n_time = projected_track_positions.shape[0]
+    projected_track_positions = projected_track_positions[(
+        np.arange(n_time), track_segment_id)]
+    edge_ids = np.array(track_graph.edges)[track_segment_id]
+
+    linear_distance = []
+
+    for projected_position, edge_id in zip(
+            projected_track_positions, edge_ids):
+        track_graph1 = track_graph.copy()
+        node1, node2 = edge_id
+        track_graph1.add_path([node1, 'projected', node2])
+        track_graph1.remove_edge(node1, node2)
+        track_graph1.nodes['projected']['pos'] = tuple(projected_position)
+
+        # calculate distance
+        for edge in track_graph1.edges(data=True):
+            track_graph1.edges[edge[:2]]['distance'] = np.linalg.norm(
+                track_graph1.node[edge[0]]['pos'] -
+                np.array(track_graph1.node[edge[1]]['pos']))
+
+        linear_distance.append(
+            nx.shortest_path_length(track_graph1, source='projected',
+                                    target=well_id, weight='distance'))
+
+    return np.array(linear_distance)
