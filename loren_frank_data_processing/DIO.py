693bd6c33780f32b602ea4938cb8750c0dd539ea
Eric Denovellis <edeno@bu.edu>
2018-01-24 13:30:25 -0500

    Include DIOs without responses

--- loren_frank_data_processing/DIO.py
+++ loren_frank_data_processing/DIO.py
@@ -8,21 +8,21 @@ from .tetrodes import get_trial_time
 def get_DIO(epoch_key, animals):
     ''''''
     animal, day, epoch = epoch_key
-    pins = get_data_structure(animals[animal], day, 'DIO', 'DIO')[
-        epoch - 1].squeeze()
+    pins = get_data_structure(
+        animals[animal], day, 'DIO', 'DIO')[epoch - 1].squeeze()
     pins_df = []
 
     for pin in pins:
-        time = pin['times'][0, 0].squeeze()
-        if time.size > 1:
-            time = pd.to_timedelta(time, unit='s')
+        time = pd.to_timedelta(pin['times'][0, 0].squeeze(), unit='s')
+        values = pin['values'][0, 0].squeeze()
+        pin_id = pin['original_id'][0, 0].item()
 
-            values = pin['values'][0, 0].squeeze()
-            pin_id = pin['original_id'][0, 0].item()
-
-            pins_df.append(
-                pd.Series(values, index=time, name=pin_id))
-    return pd.concat(pins_df, axis=1).fillna(0)
+        try:
+            series = pd.Series(values, index=time, name=pin_id)
+        except TypeError:
+            series = pd.Series(name=pin_id)
+        pins_df.append(series)
+    return pd.concat(pins_df, axis=1).fillna(0).sort_index()
 
 
 def get_DIO_indicator(epoch_key, animals, time_function=get_trial_time):
