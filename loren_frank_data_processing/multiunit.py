6f95172596a9be649f0fce94a80fa62a6d12821c
Eric Denovellis <edeno@bu.edu>
2018-06-26 14:09:09 -0400

    Exclude multiunit spike times that are greater than the time of the epoch

--- loren_frank_data_processing/multiunit.py
+++ loren_frank_data_processing/multiunit.py
@@ -144,5 +144,6 @@ def get_multiunit_indicator_dataframe(tetrode_key, animals,
         multiunit_dataframe = get_multiunit_dataframe2(tetrode_key, animals)
     time_index = np.digitize(multiunit_dataframe.index.total_seconds(),
                              time.total_seconds())
+    time_index = time_index[time_index < len(time) - 1]
     return (multiunit_dataframe.groupby(time[time_index]).mean()
             .reindex(index=time))
