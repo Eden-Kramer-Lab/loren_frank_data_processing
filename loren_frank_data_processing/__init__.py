20276b7979df22b170d2cbea5ca674fcf01694a0
Eric Denovellis <edeno@bu.edu>
2018-09-26 10:53:39 -0400

    Remove unused functions
    Functionality replaced by xarray function mfadataset

--- loren_frank_data_processing/__init__.py
+++ loren_frank_data_processing/__init__.py
@@ -8,8 +8,7 @@ from .position import (get_interpolated_position_dataframe,
                        get_position_dataframe, get_segments_df)
 from .ripples import (get_computed_consensus_ripple_times,
                       get_computed_ripples_dataframe)
-from .saving import (get_analysis_file_path, open_mfdataset,
-                     read_analysis_files, save_xarray)
+from .saving import get_analysis_file_path, save_xarray
 from .task import make_epochs_dataframe
 from .tetrodes import (get_LFP_dataframe, get_LFPs, get_trial_time,
                        make_tetrode_dataframe)
