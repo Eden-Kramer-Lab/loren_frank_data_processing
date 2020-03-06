5dcd5e344ac31d4c92ea33e7ebfd860804e5ea0e
Eric Denovellis <edeno@bu.edu>
2020-03-05 18:36:38 -0800

    Fix handling of NaNs.

--- loren_frank_data_processing/track_segment_classification.py
+++ loren_frank_data_processing/track_segment_classification.py
@@ -393,7 +393,7 @@ def calculate_linear_distance(track_graph, track_segment_id, well_id,
 
     '''
     is_nan = np.isnan(track_segment_id)
-    track_segment_id[np.isnan(track_segment_id)] = -1  # need to check
+    track_segment_id[is_nan] = 0  # need to check
     track_segment_id = track_segment_id.astype(int)
 
     track_segments = get_track_segments_from_graph(track_graph)
