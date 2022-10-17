8d0c09b38b3bb8c87002b8500c73dfe2c9e1e9a6
Eric Denovellis <edeno@bu.edu>
2022-10-17 13:52:39 -0400

    Bump version number

--- setup.py
+++ setup.py
@@ -16,7 +16,7 @@ TESTS_REQUIRE = ["pytest >= 2.7.1"]
 
 setup(
     name="loren_frank_data_processing",
-    version="0.9.27.dev0",
+    version="1.0.0",
     license="GPL-3.0",
     description=("Import data from Loren Frank lab"),
     author="Eric Denovellis",
